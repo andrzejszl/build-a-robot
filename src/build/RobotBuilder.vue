588564fff1b2a590c1036a83363e83225d1011e0
Andrzej Szlachta <andrzej.sz123@gmail.com>
2023-02-08 21:24:03 +0100

    Lifecycle hook exercise

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -60,6 +60,7 @@
 
 <script>
 import availableParts from '../data/parts';
+import createdHookMixin from './created-hook-mixin';
 
 function getPreviousValidIndex(index, length) {
   const deprecatedIndex = index - 1;
@@ -84,6 +85,7 @@ export default {
       selectedBaseIndex: 0,
     };
   },
+  mixins: [createdHookMixin],
   computed: {
     saleBorderClass() {
       return this.selectedRobot.head.onSale ?
