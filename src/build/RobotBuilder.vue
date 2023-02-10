f668c8d186f52c342d86b1ee8802e5215624a55d
Andrzej Szlachta <andrzej.sz123@gmail.com>
2023-02-10 20:47:10 +0100

    Add navigation guard

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -70,10 +70,21 @@ import CollapsableSection from '../shared/CollapsableSection.vue';
 
 export default {
   name: 'RobotBuilder',
+  beforeRouteLeave(to, from, next) {
+    if (this.addedToCart) {
+      next(true);
+    } else {
+      /* eslint no-alert: 0 */
+      /* eslint no-restricted-globals: 0 */
+      const response = confirm('You have not added your robot to your cart, are you sure you want to leave?');
+      next(response);
+    }
+  },
   components: { PartSelector, CollapsableSection },
   data() {
     return {
       availableParts,
+      addedToCart: false,
       cart: [],
       selectedRobot: {
         head: {},
@@ -109,6 +120,7 @@ export default {
       robot.base.cost;
       // eslint-disable-next-line
       this.cart.push(Object.assign({}, robot, { cost }));
+      this.addedToCart = true;
     },
   },
 };
