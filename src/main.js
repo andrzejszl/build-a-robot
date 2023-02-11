82ae83d62230a560a4300206537e0cadaa1abf9b
Andrzej Szlachta <andrzej.sz123@gmail.com>
2023-02-11 18:31:50 +0100

    Add custom pin directive

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store/index';
+import pinDirective from './shared/pin-directive';
 
 const app = createApp(App);
 app.use(router);
 app.use(store);
+app.directive('pin', pinDirective);
 app.mount('#app');
---
+++ src/shared/pin-directive.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+function applyStyle(element, binding) {
+
+};
+export default function(element, binding) {
+  Object.keys(binding.value).forEach(position => {
+    element.style[position] = binding.value[position];
+  });
+  element.style.position = 'absolute';
+};
